9e263d03b39d9cd8ec619def6253b71716d0e0a6
Trampenau, Rene <rene.trampenau@akka.eu>
2019-04-10 10:29:12 +0200

    - added fitting curve for epsilon

--- simu.m
+++ simu.m
@@ -2,7 +2,7 @@ clear all
 
 
 %% simulation
-sim.dt                          = 0.001;              % sampling rate in [s]
+sim.dt                          = 0.1;              % sampling rate in [s]
 sim.T                           = 10;               % simulated time in [s]
 
 % tractor parameter
@@ -16,64 +16,78 @@ param.tractor.psiInit           = 0;
 param.sprayer.l2                = 5.5;              % {m]
 param.sprayer.l3                = 0;                % {m]
 
-param.sprayer.alphaInit         = 0 * pi/180;       % angle between tractor and sprayer
-param.sprayer.betaInit          = 0 * pi/180;       % kink angle
-param.sprayer.psiInit           = 0 * pi/180;
+figure(2)
+clf;
+figure(3)
+clf;
+pause(0.001);
 
-index = 1;
 
-for steering = -45:1:45
-    % control
-    control.tractor.steeringAngle   = steering*pi/180;
-    control.tractor.frontWheelV     = 10;           % [m/s]
-    control.sprayer.beta            = 0;
+for iterateAlpha = -30:10:30
+    index = 1;
     
-    figure(1);
-    clf;
-    distance = 0;
+    param.sprayer.alphaInit         = iterateAlpha * pi/180;       % angle between tractor and sprayer
+    param.sprayer.betaInit          = -10 * pi/180;       % kink angle
+    param.sprayer.psiInit           = 10 * pi/180;
     
-    %% init
-    [tractor, sprayer] = initStep(param);
-    animation(0, control, tractor, sprayer);
+    for steering = -45:1:45
+        % control
+        control.tractor.steeringAngle   = steering*pi/180;
+        control.tractor.frontWheelV     = 10;           % [m/s]
+        control.sprayer.beta            = param.sprayer.betaInit;
 
-    hitchXold = tractor.hitchX;
-    hitchYold = tractor.hitchY;
-    
-    %% calculation
-    for i = 1:(sim.T/sim.dt)
-        [tractor, sprayer] = singleStep(param, control, sim, tractor, sprayer);
-        animation(1, control, tractor, sprayer);
+        figure(1);
+        clf;
+        distance = 0;
 
-        distance = distance + sprayer.ds;
-        
-        if (distance > 0.2)
-            break;
+        %% init
+        [tractor, sprayer] = initStep(param);
+        animation(0, control, tractor, sprayer);
+
+        hitchXold = tractor.hitchX;
+        hitchYold = tractor.hitchY;
+
+        %% calculation
+        for i = 1:(sim.T/sim.dt)
+            [tractor, sprayer] = singleStep(param, control, sim, tractor, sprayer);
+            animation(1, control, tractor, sprayer);
+
+            distance = distance + sprayer.ds;
+
+            if (distance > 0.2)
+                break;
+            end
+
+            %pause(sim.dt/2);
         end
-        
-        %pause(sim.dt/2);
+
+        % calculate the distance the hitch traveled
+        x(index) = steering;
+        hitchDistance(index) = sqrt((tractor.hitchX - hitchXold)^2 + ...
+                                (tractor.hitchY - hitchYold)^2);
+        hitchDirection(index) = atan2d((tractor.hitchY - hitchYold), ...
+                                   (tractor.hitchX - hitchXold));
+        distarray(index) = distance;
+
+        index = index + 1;
+
+        figure(2);
+        hold on;
+        plot(x,hitchDistance,'r');
+        plot(x,distarray,'b');
+
+        figure(3);
+        hold on;
+        plot(x,hitchDirection,'g.');
+
+        fprintf('#');
     end
     
-    % calculate the distance the hitch traveled
-    x(index) = steering;
-    hitchDistance(index) = sqrt((tractor.hitchX - hitchXold)^2 + ...
-                            (tractor.hitchY - hitchYold)^2);
-    hitchDirection(index) = atan2d((tractor.hitchY - hitchYold), ...
-                               (tractor.hitchX - hitchXold));
-    distarray(index) = distance;
-    
-    index = index + 1;
-
-    figure(2);
-    clf;
-    plot(x,hitchDistance,'r');
-    plot(x,distarray,'b');
-
-    figure(3);
-    clf;
-    plot(x,hitchDirection,'g');
-    
-    fprintf('#');
+    fprintf('\n');
+    pause(0.001);
 end
 
-%min(hitchDistance)
-%max(hitchDistance)
+figure(3)
+fittingCurve = polyfit(x,hitchDirection,5)
+plot(x,polyval(fittingCurve,x));
+%fprintf('%d * x^3 + %d * x^2 + %d * x^1 + %d\n', fittingCurve(4), fittingCurve(3), fittingCurve(2), fittingCurve(1));
