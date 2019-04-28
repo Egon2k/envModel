55f0e561eb27785ba7d17b90317fd9b49fc6ead3
René Trampenau <rtrampenau@gmail.com>
2019-04-28 22:39:51 +0200

    - first controller attempt - fix alpha calculation first (sign)

--- envModel.m
+++ envModel.m
@@ -9,7 +9,7 @@ param.tractor.hitchLength       = 0.72;             % [m]
 param.tractor.psiInit           = 0 * pi/180;
 
 %% control
-radius                          = 10;               %[m]
+radius                          = 15;               %[m]
 control.tractor.steeringAngle   = -atan(param.tractor.wheelbase/radius);
 control.tractor.frontWheelV     = 2;                % [m/s]
 control.sprayer.beta            = 0 *  pi/180;
@@ -24,7 +24,7 @@ param.sprayer.betaInit          = 0 * pi/180;       % kink angle
 
 %% simulation
 sim.dt                          = 0.01;             % sampling rate in [s]
-sim.T                           = 4;                % simulated time in [s]
+sim.T                           = 20;                % simulated time in [s]
 
 %% init
 [tractor, sprayer] = initStep(param);
@@ -32,6 +32,11 @@ animation(0, control, tractor, sprayer);
 
 distance = 0;
 i_start = 0;
+
+TRANS_DELAY = 10;
+delay = zeros(1,TRANS_DELAY+1);
+delayIndex = 1;
+closestIndex = 0;
 
 %% calculation
 for i = 1:(sim.T/sim.dt)
@@ -43,14 +48,33 @@ for i = 1:(sim.T/sim.dt)
         
         distance = 0;
         animation(1, control, tractor, sprayer);
+        
+        delay(delayIndex) = sprayer.alpha;
+        delayIndex = delayIndex + 1;
+        if (delayIndex > TRANS_DELAY)
+            delayIndex = delayIndex - TRANS_DELAY + 1;
+        end
+        
+        closestIndex = delayIndex - 4;
+        if (closestIndex < 1)
+            closestIndex = closestIndex + TRANS_DELAY;
+        end
+        control.sprayer.beta = 0.85*delay(closestIndex);        
+        
+        
         pause((i-i_start) * sim.dt/4);
         i_start = i;
     end
     
-%     figure(2);
-%     hold on;
-%     plot(i,sprayer.alpha*180/pi,'gx');
-%     plot(i,tractor.psi*180/pi,'bo');
+    if i == 1000  control.tractor.steeringAngle = 10 * pi/180; end
+    if i == 3000  control.tractor.steeringAngle = 30 * pi/180; end
+    if i == 4000  control.tractor.steeringAngle = 40 * pi/180; end
+    
+    
+    figure(2);
+    hold on;
+    plot(i,sprayer.alpha*180/pi,'gx');
+    plot(i,control.sprayer.beta*180/pi,'bo');
 %     plot(i,sprayer.psi*180/pi,'ro');
 
 end
