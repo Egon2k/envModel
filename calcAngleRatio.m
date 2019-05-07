09d29b7c69a807b4aef11e697b596fb5b0335940
Trampenau, Rene <rene.trampenau@akka.eu>
2019-05-07 16:11:19 +0200

    - made the calcAngleRatio function capable for vectors

--- calcAngleRatio.m
+++ calcAngleRatio.m
@@ -1,8 +1,8 @@
 function [ factor ] = calcAngleRatio( alpha, l1, l2 )
 % Calculates ratio between alpha and beta in a perfect cycle.
     if alpha ~= 0
-        a = (l1^2 + 2 * l1 * l2 .* cos(alpha) + l2^2) ./ ...
-                  (2 * (l1 + l2 .* cos(alpha)));
+        a = (l1.^2 + 2 .* l1 * l2 .* cos(alpha) + l2.^2) ./ ...
+                  (2 .* (l1 + l2 .* cos(alpha)));
 
         beta = asin((a - l1) ./ a .* sin(alpha));
 
